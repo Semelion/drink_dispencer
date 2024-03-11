2b7647d753debf3790a52d3a2c1992f97b16e15b
Semyon Trafnyak <m2202962@edu.misis.ru>
2024-03-11 17:58:03 +0300

    esp code part

--- rksp_mqtt_new_lib/pwd.h
+++ rksp_mqtt_new_lib/pwd.h
@@ -1,7 +1,6 @@
-#define ssid  "pochemoo"
-#define password  "12344321"
+#define ssid  "DOM"
+#define password  "14272717"
 #define mqtt_server "broker.emqx.io"
 #define mqtt_port 1883
 
-#define permission_topic "semelion/permission"
 #define value_topic "semelion/value"
--- rksp_mqtt_new_lib/rksp_mqtt_new_lib.ino
+++ rksp_mqtt_new_lib/rksp_mqtt_new_lib.ino
@@ -1,14 +1,32 @@
 #include <ArduinoMqttClient.h>
 #include <ESP8266WiFi.h>
-
 #include "pwd.h"
+#include <Adafruit_NeoPixel.h>
+#include <Servo.h>
+
+Servo servo;
+
+#define ml_sec 10
+
+#define pump_Pin 16
+#define valve_Pin 5
+
+#define switch1 4
+#define switch2 0
+#define switch3 15
+
+#define strip_pin 14
+#define NUMPIXELS 3
+Adafruit_NeoPixel pixels(NUMPIXELS, strip_pin, NEO_GRB + NEO_KHZ800);
 
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
 
-void connect2wifiAndServer(){
+int glass_pos[3] = {0,0,0};
+
+void connect2wifiAndServer() {
   // attempt to connect to Wifi network:
-  if (WiFi.status() != WL_CONNECTED){
+  if (WiFi.status() != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
     while (WiFi.begin(ssid, password) != WL_CONNECTED) {
@@ -16,7 +34,7 @@ void connect2wifiAndServer(){
       Serial.print(".");
       delay(5000);
     }
-    
+
     Serial.println("You're connected to the network");
     Serial.println();
   }
@@ -33,47 +51,143 @@ void connect2wifiAndServer(){
 
   Serial.println("You're connected to the MQTT broker!");
   Serial.println();
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+    pixels.show();
+  }
+  servo.write(180);
+  delay(200);
+  servo.write(90);
+  delay(200);
+  servo.write(0);
+
 }
 
 void setup() {
+  pinMode(pump_Pin, OUTPUT);
+  pinMode(valve_Pin, OUTPUT);
+
+  pinMode(switch1, INPUT);
+  pinMode(switch2, INPUT);
+  pinMode(switch3, INPUT);
+
   //Initialize serial and wait for port to open:
   Serial.begin(9600);
+  pixels.begin();
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-    
+  pixels.clear();
+  analogWriteResolution(16);
+  servo.attach(2, 544, 2444); //D4
+  servo.write(0);
+  delay(100);
+  servo.write(180);
+
   connect2wifiAndServer();
-  
-  // set the message receive callback
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+    pixels.show();
+  }
+
   mqttClient.onMessage(onMqttMessage);
-
-
-  // subscribe to a topic
-  mqttClient.subscribe(permission_topic);
   mqttClient.subscribe(value_topic);
-
-  // topics can be unsubscribed using:
-  // mqttClient.unsubscribe(topic);
-
 }
 
 void loop() {
   mqttClient.poll();
+//  if(digitalRead(switch1))
+//    Serial.println("ddd");
+  if(!digitalRead(switch1)){
+    if(glass_pos[0] == 0){
+      glass_pos[0] = 1;
+      pixels.setPixelColor(0, pixels.Color(150, 0, 0));
+    }
+  }else{
+    glass_pos[0] = 0;
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+  }
+  
+  if(!digitalRead(switch2)){
+    if(glass_pos[1] == 0){
+      glass_pos[1] = 1;
+      pixels.setPixelColor(1, pixels.Color(150, 0, 0));
+    }
+  }else{
+    glass_pos[1] = 0;
+    pixels.setPixelColor(1, pixels.Color(0, 0, 0));
+  }
+
+  if(!digitalRead(switch3)){
+    if(glass_pos[2] == 0){
+      glass_pos[2] = 1;
+      pixels.setPixelColor(2, pixels.Color(150, 0, 0));
+    }
+  }else{
+    glass_pos[2] = 0;
+    pixels.setPixelColor(2, pixels.Color(0, 0, 0));
+  }
+ 
+  pixels.show();
 }
 
 
 void onMqttMessage(int messageSize) {
   // we received a message, print out the topic and contents
+  String top = mqttClient.messageTopic();
   Serial.println("Received a message with topic '");
-  Serial.print(mqttClient.messageTopic());
-  Serial.print("', length ");
+  Serial.print(top);
+  Serial.print(", length ");
   Serial.print(messageSize);
   Serial.println(" bytes:");
-
+  String input = "";
   // use the Stream interface to print the contents
   while (mqttClient.available()) {
-    Serial.print((char)mqttClient.read());
+    input += (char)mqttClient.read();
   }
-  Serial.println();
+  Serial.println(input);
+  
+  if(top == value_topic){
+    Serial.println("START");
+    int val = input.toInt();
+    if(glass_pos[0] == 1){
+      servo.write(45);
+      delay(50);
+      digitalWrite(pump_Pin, HIGH);
+      digitalWrite(valve_Pin, HIGH);
+      delay(val*ml_sec);
+      digitalWrite(pump_Pin, LOW);
+      digitalWrite(valve_Pin, LOW);
+      pixels.setPixelColor(0, pixels.Color(0, 150, 0));
+      glass_pos[0] = 2;
+      pixels.show();
+    }
+    if(glass_pos[1] == 1){
+      servo.write(90);
+      delay(50);
+      digitalWrite(pump_Pin, HIGH);
+      digitalWrite(valve_Pin, HIGH);
+      delay(val*ml_sec);
+      digitalWrite(pump_Pin, LOW);
+      digitalWrite(valve_Pin, LOW);
+      pixels.setPixelColor(1, pixels.Color(0, 150, 0));
+      glass_pos[1] = 2;
+      pixels.show();
+    }
+    if(glass_pos[2] == 1){
+      servo.write(135);
+      delay(50);
+      digitalWrite(pump_Pin, HIGH);
+      digitalWrite(valve_Pin, HIGH);
+      delay(val*ml_sec);
+      digitalWrite(pump_Pin, LOW);
+      digitalWrite(valve_Pin, LOW);
+      pixels.setPixelColor(2, pixels.Color(0, 150, 0));
+      glass_pos[2] = 2;
+      pixels.show();
+    }
+    servo.write(0);
+    
+  }
   Serial.println();
 }
